1baf77829622b17ca323525931e14eab8272920d
KJCole14 <kabahjcole@gmail.com>
2022-09-15 08:56:10 -0400

    Completed Easy GI

--- app.js
+++ app.js
@@ -2,4 +2,14 @@
 var num1 = 6;
 
 var num2 = 2;
-console.log(num1 - num2);
+console.log("The difference between " + num1 + " " + "and " + num2 + " is " + (num1 - num2));
+
+//Easy
+var Michael, Sam;
+Sam = 3;
+Michael = 7;
+if (Michael > Sam) {
+    console.log("The name Michael is longer that Sam by " + (Michael - Sam) + " Characters");
+}
+
+//Medium
